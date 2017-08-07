06f2b00703959d1ce4efc2874752dca9bfb3d6cb
Jean-PaulD <jpdebuys@gmail.com>
2017-08-07 22:54:06 +0200

    V0.95

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -5,4 +5,114 @@ package domain;
  */
 public class Item {
 
+    private String itemId;
+    private String username;
+    private String description;
+    private String itemName;
+    private double itemValue;
+    private String currentBidOwner;
+    private String comment;
+
+    private Item(){}
+
+    public Item(Builder builder) {
+        this.itemId = builder.itemId;
+        this.username = builder.username;
+        this.description = builder.description;
+        this.itemName = builder.itemName;
+        this.itemValue = builder.itemValue;
+        this.currentBidOwner = builder.currentBidOwner;
+        this.comment = builder.comment;
+
+    }
+
+    public static class Builder{
+
+
+        private String itemId;
+        private String username;
+        private String description;
+        private String itemName;
+        private double itemValue;
+        private String currentBidOwner;
+        private String comment;
+
+
+
+
+        public Builder itemId(String value){
+            this.itemId = value;
+            return this;
+        }
+
+        public Builder username(String value){
+            this.username = value;
+            return this;
+        }
+
+        public Builder description(String value){
+            this.description = value;
+            return this;
+        }
+
+        public Builder itemName(String value){
+            this.itemName = value;
+            return this;
+        }
+
+        public Builder itemValue(double value){
+            this.itemValue = value;
+            return this;
+        }
+
+        public Builder currentBidOwner(String value){
+            this.currentBidOwner = value;
+            return this;
+        }
+
+        public Builder comment(String value){
+            this.comment = value;
+            return this;
+        }
+
+
+        public Item build(){
+            return new Item(this);
+        }
+    }
+
+
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getItemValue() {
+        return itemValue;
+    }
+
+    public String getCurrentBidOwner() {
+        return currentBidOwner;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+
+
+
+
 }
--- src/main/java/domain/ReportUser.java
+++ src/main/java/domain/ReportUser.java
@@ -4,4 +4,58 @@ package domain;
  * Created by Jean-Paul D on 2017-08-07.
  */
 public class ReportUser {
+
+    private String id;
+    private String username;
+    private int numberOfReports;
+
+    private ReportUser(){}
+
+    public ReportUser(Builder builder) {
+        this.id = builder.id;
+        this.username = builder.username;
+        this.username = builder.username;
+        this.numberOfReports = builder.numberOfReports;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getNumberOfReports() {
+        return numberOfReports;
+    }
+
+    public static class Builder{
+
+        private String id;
+        private String username;
+        private int numberOfReports;
+
+        public Builder id(String value){
+            this.id = value;
+            return this;
+        }
+
+        public Builder username(String value){
+            this.username = value;
+            return this;
+        }
+
+        public Builder numberOfReports(int value){
+            this.numberOfReports = value;
+            return this;
+        }
+
+        public ReportUser build(){
+            return new ReportUser(this);
+        }
+    }
+
+
+
 }
